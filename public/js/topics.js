b517815ba4a0920a0f20c8e81c2612ae550b03ae
JaiSharmaTech <kartikeykakaria5@gmail.com>
2022-11-27 08:54:07 +0530

    done with the main css of topics page

--- public/js/topics.js
+++ public/js/topics.js
@@ -8,10 +8,10 @@ fetch("/", params)
 .then(rep=>rep.json())
 .then(data=>{
     const Topics = document.getElementById("Topics")
-    data.forEach((element, index)=>{
+    data.forEach(element=>{
         const topic = document.createElement("li")
         topic.classList.add("topic");
-        topic.innerHTML = `<a href="/topics/${element.name}">${index+1} ${element.name}</a>`;
+        topic.innerHTML = `<li class="topic"><a href="/topics/${element.name}">${element.name}</a></li>`;
         Topics.appendChild(topic)
     })
 })
