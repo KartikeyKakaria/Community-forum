a9faf7cd5d702a670206b390e0ee7b75785462fd
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2022-12-21 05:24:58 +0000

    added functionality for password hashing

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const User = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    address:String,
-    age:{
-        type:String,
-        required:true
-    },
-    number:{
-        type:Number,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: String,
+  age: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-})
-const Register = new mongoose.model("user",User);
+User.pre("save", async function(next){
+  this.password = await bcrypt.hash(this.password, 4);
+  next();
+});
+const Register = new mongoose.model("user", User);
 module.exports = Register;
